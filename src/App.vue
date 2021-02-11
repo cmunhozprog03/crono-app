053ef8833c2d26d9996f402019559095cbed0026
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-02-11 00:14:52 -0300

    Cretae layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/cronometro.png" class="img" alt="">
+    
+    <a class="timer">00:00,00</a>
+
+    <div class="areaBtn">
+      <button class="botao">VAI</button>
+      <button class="botao">LIMPAR</button>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .img{
+    width: 450px;
+    height: 450px;
+    padding-top: 70px;
+  }
+  .timer{
+    color: #ffffff;
+    font-size: 70px;
+    margin-top: -230px;
+  }
+  .areaBtn{
+    margin-top: 155px;
+    display: flex;
+  }
+  .botao{
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    width: 150px;
+    background-color: #FFF;
+    font-size: 20px;
+    border:0;
+    border-radius: 5px;
+    text-align: center;
+    margin-left: 15px;
+    margin-right: 15px;
+    padding: 6px;
+    cursor:pointer;
+  }
+  .botao:hover{
+    opacity: 0.7;
+    transition: all 0.50s;
+  }
 </style>
